ecf365bd0ca373fad4ee3879d67d9c7884eb8055
Tiago Bastos <tbbgoncalves@outlook.com>
2024-02-01 02:39:43 -0300

    commit inicial

---
+++ src/main/java/br/com/alura/ScreenSoundMusicas/ScreenSoundMusicasApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.ScreenSoundMusicas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenSoundMusicasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenSoundMusicasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alura/ScreenSoundMusicas/ScreenSoundMusicasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.ScreenSoundMusicas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenSoundMusicasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
